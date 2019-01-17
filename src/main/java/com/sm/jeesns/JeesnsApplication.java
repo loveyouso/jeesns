41072b14920f2854558f734e227bf524ff7b10b8
Iloveyouso <wss535242100@gmial.com>
2019-01-17 22:55:40 +0800

    Initial commit

---
+++ src/main/java/com/sm/jeesns/JeesnsApplication.java
@@ -0,0 +1,14 @@
+package com.sm.jeesns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeesnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeesnsApplication.class, args);
+	}
+
+}
+
